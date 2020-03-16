7719f508c827daa8f2bc25fe5e5986dbc83f4857
Taspon Gonggiatgul <tagonggiatgul@ucdavis.edu>
2020-03-15 18:19:36 -0700

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+#Random Matrix Generator
+
+##Usage
+Go into the repo directory and cd into './src'
+run the following command
+'./generate.py' with the following flag:
+- -n with the # of files you want to generate, default = 8.
+- -z with the matrix size of N x N, default = 8.
+- -b with the maximum bit of each matrix element, default = 8.
+
+##Output
+All the generated matrix will be stored inside './matrix'.
---
+++ src/generate.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+import numpy as np
+import sys
+import shutil
+import os
+
+
+def main():
+    n = 0
+    z = 0
+    b = 0
+    n_detect = False
+    z_detect = False
+    b_detect = False
+
+    for i in sys.argv:
+        if i == "--help":
+            print("test.py -n -z -b")
+            print("     -n, where n is the # of files we want to generate")
+            print("     -z, where z is the matrix size of z x z")
+            print("     -b, where b is the maximum bit of each matrix element")
+            exit(-1)
+        elif n_detect:
+            n = int(i)
+            n_detect = False
+        elif z_detect:
+            z = int(i)
+            z_detect = False
+        elif b_detect:
+            b = int(i)
+            b_detect = False
+        else:
+            if n_detect == False and i == "-n":
+                n_detect = True
+            elif z_detect == False and i == "-z":
+                z_detect = True
+            elif b_detect == False and i == '-b':
+                b_detect = True
+
+    if n == 0 or z == 0 or b == 0:
+        print("Error Command!, please \"--help\" for usage instruction")
+        exit(-1)
+
+    try:
+        os.rmdir("./matrix")
+    except:
+        decision = input("The matrix file is not empty, wish to delete all files inside? (Y/n):")
+        if decision == '':
+            shutil.rmtree("./matrix")
+        else:
+            print("please sort through your matrix before generating a new set")
+            exit(-1)
+
+    try:
+        os.mkdir("../matrix")
+    except:
+        print("folder already created")
+
+    os.chdir("../matrix")
+
+    file_name = "Matrix"
+
+    for i in range(n):
+        temp = np.random.randint(2 ** b, size=(z * z))
+        name = file_name + str(i + 1) + ".txt"
+        argument = "{0:0" + str(b) + "b}"
+        fd = open(name, 'w')
+
+        for k in range(z * z):
+            if k == z * z - 1:
+                fd.write("%s" % argument.format(temp[k]))
+            else:
+                fd.write("%s\n" % argument.format(temp[k]))
+
+        fd.close()
+
+        print("finished creating %d matrix file" % (i + 1))
+
+    print("SUCCESS: Finished creating all files ....")
+
+
+if __name__ == "__main__":
+    main()
